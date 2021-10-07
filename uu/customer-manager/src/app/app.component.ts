861ef6fc1ef83868ccbb7a576792b656b3cf4027
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-10-07 13:14:29 +0200

    first

--- uu/customer-manager/src/app/app.component.ts
+++ uu/customer-manager/src/app/app.component.ts
@@ -3,26 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container">
-    <div class="panel panel-default">
-    <div class="panel-heading">
-    <h1 [style.color]="nameColor" [class.isActive]="isActive">{{ customer.name.toUpperCase() }}</h1>
-    </div>
-    <div class="panel-body">
-      <div class="row">
-        <div class="col-sm-6">
-          <p>{{ customer.description }}</p>
-          <p>Wiek: {{ customer.age }}</p>
-          <p>Adres: {{ customer.age }}</p>
-        </div>
-      </div>
-    </div>
-    </div>
-  </div>
-    
+  <h1 [style.color]="nameColor" [class.isActive]="isActive">{{ name.toUpperCase() }}</h1>
+  <img [src]="photoURL" alt="">
   `,
-  styles: []
+  styles: [
+    '.isActive {text-decoration: underline;}'
+  ]
 })
 export class AppComponent {
-  title = 'customer-manager';
+  name: string = "Jan Kowalski";
+  photoURL: string = "assets/images/customer.png";
+  nameColor: string = "blue";
+  isActive: boolean = true;
+
+  constructor(){
+    setTimeout(()=>{
+      this.name = "Wojciech";
+    },3000);
+  }
 }
