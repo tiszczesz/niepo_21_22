36e629ec5a478f5f24653528cad63377e96b2e85
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-11-18 14:54:51 +0100

    Update Form2.cshtml.cs

--- 2tip_main/2tip_web/WebRPWithForm/WebRPWithForm/Pages/Form2.cshtml.cs
+++ 2tip_main/2tip_web/WebRPWithForm/WebRPWithForm/Pages/Form2.cshtml.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebRPWithForm.Models;
 
 namespace WebRPWithForm.Pages
 {
     public class Form2Model : PageModel
     {
+        [BindProperty]
+        public User MyUser { get; set; }
         public void OnGet()
         {
+        }
+
+        public void OnPost() {
+            var resut = MyUser;
         }
     }
 }
