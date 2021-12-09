8cecc05f63d5edf45ee14a279e62e62c93f023f2
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-12-09 09:59:51 +0100

    ff

---
+++ 3tiGim_obiek/TempClassGim1/TempClassGim1/Pair.h
@@ -0,0 +1,7 @@
+#pragma once
+struct Pair
+{
+	int a, b;
+};
+
+
--- 3tiGim_obiek/TempClassGim1/TempClassGim1/Source.cpp
+++ 3tiGim_obiek/TempClassGim1/TempClassGim1/Source.cpp
@@ -3,15 +3,22 @@
 #include <string>
 #include "Container.h"
 #include <vector>
+#include "Pair.h"
 using namespace std;
 
 int main() {
 	vector<string> v1{"ala", "mela", "Bella", "ciela"};
+	Pair p1{ 12,6 };
+	Pair p2{ 3,67 };
+	Pair p3{ 44,67 };
+	vector<Pair> pp {p1, p2, p3};
 	vector<int> numbers{ 12,6,8,9,0,23 };//na stosie ->w momencie kompliacji
 	Container<string>* c1 = new Container<string>(v1);//na stercie przy uruchomieniu
 	Container<int> liczby(numbers);
+	Container<Pair>*  pary = new Container<Pair>(pp);
 	(*c1).ShowAll();
 	c1->ShowAll();
+	//pary->ShowAll();
 	delete c1;
 	c1 = nullptr;
 	if(c1!=nullptr) {
