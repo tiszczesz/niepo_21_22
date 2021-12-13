c85414c003f01b64dbd93c1039d80c59620a7a20
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-12-13 08:36:35 +0100

    Create TextInfo.cs

---
+++ 3ti_gim_desktopowe/FileOpenSave_gim_3ti/FileOpenSave_gim_3ti/TextInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOpenSave_gim_3ti
+{
+    public class TextInfo {
+        private List<string> _lines;
+
+        public TextInfo(List<string> lines) {
+            _lines = lines;
+            Lines = _lines.Count;
+        }
+
+        public int Lines { get; set; }
+        public int Chars { get; set; }
+        public int Letters { get; set; }
+
+        public int GetNotEmptyLines() {
+            return 0;//todo
+        }
+    }
+}
