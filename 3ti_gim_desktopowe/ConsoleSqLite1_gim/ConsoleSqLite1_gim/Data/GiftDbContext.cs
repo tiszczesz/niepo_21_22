80558e0ea78d56a4e8c378f563d7b03a57ee8304
pszczesz <pszczesz@gmail.com>
2021-12-20 09:10:56 +0100

    Update GiftDbContext.cs

--- 3ti_gim_desktopowe/ConsoleSqLite1_gim/ConsoleSqLite1_gim/Data/GiftDbContext.cs
+++ 3ti_gim_desktopowe/ConsoleSqLite1_gim/ConsoleSqLite1_gim/Data/GiftDbContext.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleSqLite1_gim.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ConsoleSqLite1_gim.Data
 {
-    class GiftDbContext
+    public class GiftDbContext : DbContext
     {
+        public DbSet<Gift> Gifts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Data Source=myGifts_gim.db");
+        }
     }
 }
