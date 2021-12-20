86b1b60358fb401f33c18cba021e37884791eafe
pszczesz <pszczesz@gmail.com>
2021-12-20 14:14:23 +0100

    Update cw20.js

--- 3ti_inf_gim/cw20/cw20.js
+++ 3ti_inf_gim/cw20/cw20.js
@@ -9,11 +9,25 @@ class User{
         return `${this.firstname} ${this.lastname} wiek: ${this.age} data rejestracji: ${this.dateOfRe.toLocaleDateString()}`;
     }
 }
-
+class ToHtml{
+    static UsersToTable(data){
+        let html = "<table><tr><th>Imię</th><th>Nazwisko</th><th>Wiek</th><th>Data rejestracji</th></tr>";
+        for (const user of data) {
+            html += `<tr>
+                <td>${user.firstname}</td>
+                <td>${user.lastname}</td>
+                <td>${user.age}</td>
+                <td>${user.dateOfRe.toLocaleDateString()}</td>
+            </tr>`;
+        }
+        return html+"</table>";
+    }
+}
 const users = [
     new User("Antoni","Hajduk",23),
     new User("Monika","Grabska",25,new Date('2020-03-12'))
     
 ];
-document.querySelector(".root").innerHTML = `<div class='userInfo'>${users[0].showString()}</div>
-                        <div class='userInfo'>${users[1].showString()}</div>`;
+// document.querySelector(".root").innerHTML = `<div class='userInfo'>${users[0].showString()}</div>
+//                         <div class='userInfo'>${users[1].showString()}</div>`;
+document.querySelector(".root").innerHTML = ToHtml.UsersToTable(users);
