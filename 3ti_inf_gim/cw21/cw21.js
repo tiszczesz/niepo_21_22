7b8f5025393b9f4b6ac7aca97460bd677427b00f
pszczesz <pszczesz@gmail.com>
2021-12-21 14:06:09 +0100

    vv

---
+++ 3ti_inf_gim/cw21/cw21.js
@@ -0,0 +1,16 @@
+class Dish{
+    constructor(name,price,quantity){
+        this.name=name;
+        this.quantity=quantity;
+        this.price=price;
+    }
+}
+class ToHtml{
+    static ToTable(tabOfDishes){
+
+    }
+}
+
+document.querySelector("#addNew").onclick = function(e){
+    //todo
+}
