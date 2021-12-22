16f231e3a90950012453b841cb624c80b668457e
pszczesz <pszczesz@gmail.com>
2021-12-22 13:12:14 +0100

    Update cw21.js

--- 3ti_inf_gim/cw21/cw21.js
+++ 3ti_inf_gim/cw21/cw21.js
@@ -5,12 +5,27 @@ class Dish{
         this.price=price;
     }
 }
+const data = [
+    new Dish("Barszcz czerwony",5.99,3),
+    new Dish("Karp smażony",4.99,3)
+];
 class ToHtml{
-    static ToTable(tabOfDishes){
-
+    static ToTable(tabOfDishes,table){
+        let lp = 1;
+        for (const item of tabOfDishes) {
+            let tr = this.createRow(item,lp++)
+            table.appendChild(tr);
+        }
+    }
+    static createRow(dish,lp){
+        let tr = document.createElement("tr");
+        tr.innerHTML = `<td>${lp}</td><td>${dish.name}</td><td>${dish.price} PLN</td>
+                <td>${dish.quantity}</td>`;
+        return tr;
     }
 }
-
+let table = document.querySelector("#list");
+ToHtml.ToTable(data,table);
 document.querySelector("#addNew").onclick = function(e){
     //todo
 }
