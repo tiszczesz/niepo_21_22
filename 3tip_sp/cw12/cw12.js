89a84498c8e024940687614453662dfa723654b5
pszczesz <pszczesz@gmail.com>
2022-01-03 11:29:53 +0100

    ff

--- 3tip_sp/cw12/cw12.js
+++ 3tip_sp/cw12/cw12.js
@@ -1,11 +1,21 @@
 document.querySelector('#search').onclick = function () {
+    document.querySelector("#result").innerHTML = "";
   const imiona = Array.from(document.querySelector('.tosearch').children);
-  const imiona2 = document.querySelectorAll('.tosearch>li');
- 
+  //const imiona2 = document.querySelectorAll('.tosearch>li');
 
-  const result = imiona.filter(function(elem){
-      return elem.innerText.length>3;
+  const minLength = parseInt(document.querySelector('#length').value);
+  const result = imiona.filter(function (elem) {
+    return elem.innerText.length >= minLength;
   });
-
+  document.querySelector("#result").appendChild(generList(result));
   console.log(result);
 };
+function generList(data){
+    const ol = document.createElement("ol");
+    let html = "";
+    for (const elem of data) {
+        html += `<li>${elem.innerText}</li>`;
+    }
+    ol.innerHTML = html;
+    return ol;
+}
