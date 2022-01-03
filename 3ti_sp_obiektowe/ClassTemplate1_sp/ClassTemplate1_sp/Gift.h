eab81d9fa198bdd440861d118c264d7716f137dd
pszczesz <pszczesz@gmail.com>
2022-01-03 14:57:54 +0100

    Update Gift.h

--- 3ti_sp_obiektowe/ClassTemplate1_sp/ClassTemplate1_sp/Gift.h
+++ 3ti_sp_obiektowe/ClassTemplate1_sp/ClassTemplate1_sp/Gift.h
@@ -11,5 +11,15 @@ public:
 	Gift(); //todo
 	Gift(std::string name, std::string owner, double price); //todo
 	//todo
+	std::string GetName() {
+		return name;
+	}
+	double GetPrice() {
+		return price;
+	}
+	std::string GetOwner() {
+		return owner;
+	}
+	
 };
 
