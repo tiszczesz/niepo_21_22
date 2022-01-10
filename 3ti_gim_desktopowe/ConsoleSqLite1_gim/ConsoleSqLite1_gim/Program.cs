0c3b8c5c89f870bb5abf2759d15560e4c1b4a9f9
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-01-10 10:18:55 +0100

    dd

--- 3ti_gim_desktopowe/ConsoleSqLite1_gim/ConsoleSqLite1_gim/Program.cs
+++ 3ti_gim_desktopowe/ConsoleSqLite1_gim/ConsoleSqLite1_gim/Program.cs
@@ -9,14 +9,14 @@ namespace ConsoleSqLite1_gim
         static void Main(string[] args) {
            
             GiftDbContext db = new GiftDbContext();
-          //  db.Database.EnsureDeleted();
-         //   db.Database.EnsureCreated();
+            //db.Database.EnsureDeleted();
+            //db.Database.EnsureCreated();
             GiftRepo repo = new GiftRepo();
              repo.ShowAll();
-            Gift g = new Gift {Name = "rrr", Owner = "gggg", Price = 23.7M};
-           db.Gifts.Add(g);
-           db.SaveChanges();
-           repo.ShowAll();
+           // Gift g = new Gift {Name = "rrr", Owner = "gggg", Price = 23.7M};
+           //db.Gifts.Add(g);
+           //db.SaveChanges();
+           //repo.ShowAll();
             Console.ReadKey();
         }
     }
