b31ab4a0a523ca190681afe0b0442e2fc2f5580d
pszczesz <pszczesz@gmail.com>
2022-02-01 15:35:52 +0100

    dd

--- 3tip_sp/cw13/cw13.js
+++ 3tip_sp/cw13/cw13.js
@@ -19,5 +19,28 @@ function ShowBook(){
     return book1.show();
 }
 
+const Contact = {
+    imie:"Roman",
+    nazwisko: "Tomera",
+    telefon: "2345677",
+    email: "ffff@gmail.com",
+    info: function(){
+        return `<div style="color:green;border:solid 1px black;">
+        Kontakt: ${this.imie} ${this.nazwisko} tel. ${this.telefon} ${this.email}
+        </div>`;
+    }
+}
+const Contact2 = {
+    imie:"Monika",
+    nazwisko: "Rybicka",
+    telefon: "2345677",
+    email: "ffff@gmail.com",
+    info: function(){
+        return `<div style="color:blue;border:solid 1px black;">
+        Kontakt: ${this.imie} ${this.nazwisko} tel. ${this.telefon} ${this.email}
+        </div>`;
+    }
+}
+document.querySelector("#ex2").innerHTML = Contact.info()+Contact2.info();
 //stworzyc obiekt Contact z polami imie, nazwisko, telefon, email
 //oraz metode info wyswietlajaca na stronie informacje o kontakcie
