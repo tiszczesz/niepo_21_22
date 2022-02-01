bee88823fe3a3402af8a2cc870ed62e1951e6b3d
pszczesz <pszczesz@gmail.com>
2022-02-01 15:52:00 +0100

    ssdd

--- 3tip_sp/cw13/cw13.js
+++ 3tip_sp/cw13/cw13.js
@@ -34,6 +34,7 @@ const Contact2 = {
     imie:"Monika",
     nazwisko: "Rybicka",
     telefon: "2345677",
+    rrr: 553535353535,
     email: "ffff@gmail.com",
     info: function(){
         return `<div style="color:blue;border:solid 1px black;">
@@ -42,5 +43,23 @@ const Contact2 = {
     }
 }
 document.querySelector("#ex2").innerHTML = Contact.info()+Contact2.info();
+
+function Book(title,author,price){
+    this.title = title;
+    this.author = author;
+    this.price = price;
+    this.show = function(){
+        return `<p>
+        Informacje o książce:<br /> 
+        tytuł: ${this.title} <br />
+        autor: ${this.author} <br />
+        cena: ${this.price} zł <br />
+        </p>`;
+    }
+}
+const b1 = new Book("Wędkarstwo","Tomasz Rybka",45.99); // const zmienna = new Funkcja()
+const b2 = new Book("Kucharska","Renata Rybka",35.99);
+console.log(b1,b2,Contact2);
+document.querySelector("#ex3").innerHTML = b1.show()+b2.show();
 //stworzyc obiekt Contact z polami imie, nazwisko, telefon, email
 //oraz metode info wyswietlajaca na stronie informacje o kontakcie
