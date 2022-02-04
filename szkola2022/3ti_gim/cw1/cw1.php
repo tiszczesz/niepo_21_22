bd6fe0ca23104715d79515a21670fd514a5b6bc0
pszczesz <pszczesz@gmail.com>
2022-02-04 13:20:07 +0100

    gg

--- szkola2022/3ti_gim/cw1/cw1.php
+++ szkola2022/3ti_gim/cw1/cw1.php
@@ -7,12 +7,36 @@
     <title>Document</title>
 </head>
 <body>
+   <h1>Informacje o echo</h1>
     <?php
-        echo "<h3>Hello from PHP</h3>";
-       // phpinfo();
         $a = 12;
-        $b = [12,67,"dddddd",true];
-        var_dump($b);
+        var_dump($a);
+        echo "<h3>Hello from PHP {$a}</h3>"; //jest wolniej ale z przetwarzaniem np zmiennych
+        $a = "Ala ma kota";
+        echo "<h3>Hello from PHP {$a}</h3>"; //jest szybciej ale bez przetwarzania
+        var_dump($a);
+       // phpinfo();
+       
     ?>
+<h1>Informacje o typach w php</h1>
+<h3>Typ całkowity int</h3>
+   <?php
+    $calkowita = 23444;
+    $d = 100111;
+    var_dump($calkowita);
+   ?>
+   <h3>Typ zmienno przecinkowy float</h3>
+   <?php
+    $zmienno1 = 2.890;
+    $zmienno2 = 267e-2; // 267*10^-2
+    var_dump($zmienno1);
+    var_dump($zmienno2);
+    $a = 1234; // decimal number
+    $a = 0123; // octal number (equivalent to 83 decimal)
+  //  $a = 0o123; // octal number (as of PHP 8.1.0)
+    $a = 0x1A; // hexadecimal number (equivalent to 26 decimal)
+    $a = 0b11111111; // binary number (equivalent to 255 decimal)
+    $a = 1_234_567; // decimal number (as of PHP 7.4.0)
+   ?>
 </body>
 </html>
---
+++ szkola2022/3ti_gim/cw2/cw2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //dodawanie odejmowanie mnozenie i dzielenie dla $a $b w 
+        // paragrafach  -->   12.8 + 45 = 57.8
+
+        $a  = 13;
+        $b = 67.8;
+        echo '<p>'.$a.' + '.$b.' = '.($a+$b).'</p>'; // kropki łączą kolejne stringi
+    ?>
+</body>
+</html>
