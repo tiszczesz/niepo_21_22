b2670c56c16b6a61d53b134ded8917962358d705
pszczesz <pszczesz@gmail.com>
2022-02-07 13:21:33 +0100

    fff

--- 3ti_gim_web/cwJS2/cw2.js
+++ 3ti_gim_web/cwJS2/cw2.js
@@ -1,4 +1,23 @@
 $(function(){
     const lis = $('li:even');
-    console.log(lis);
+   // console.log(lis);
+
+    $("input[type=button]").each(function(){
+        $(this).click(function(){
+           $("li a").each(function(){
+                $(this).removeClass();
+           });
+            //console.log($(this).attr("id"));
+            switch($(this).attr("id")){
+                case "b1": ; break;
+                case "b2": alert("d"); break;
+                case "b3": alert("t"); break;
+                case "b4": alert("c"); break;
+                case "b5": alert("p"); break;
+                case "b6": alert("sz"); break;
+                case "b7": alert("si"); break;
+            }
+        });
+    });
+    
 });
