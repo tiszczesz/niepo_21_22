c9e40127e4b486ae4533f946c0c7787d8a853ae7
pszczesz <pszczesz@gmail.com>
2022-02-14 13:23:10 +0100

    dd

--- 3ti_gim_web/cwJS3/cw3.js
+++ 3ti_gim_web/cwJS3/cw3.js
@@ -1,5 +1,10 @@
 $(function(){
     const checks = $("[name='elem1[]']");
     console.log(checks);
-    
+    checks.each(function(){
+        $(this).on("click",function(){    //$(this).click(function(){....})
+            console.log($(this).val());
+            $(this).next().toggleClass("check-click");
+        });
+    });
 });
