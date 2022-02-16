6dea16baa4c510af1a264e7b45ff9fd901026d30
pszczesz <pszczesz@gmail.com>
2022-02-16 11:27:54 +0100

    bb

--- 3ti_gim_web/cwJS3/cw3.js
+++ 3ti_gim_web/cwJS3/cw3.js
@@ -7,4 +7,15 @@ $(function(){
             $(this).next().toggleClass("check-click");
         });
     });
+    $("#elem2").on("focus",function(){
+        console.log("focus");
+        $(this).css({"backgroundColor":"yellow"});
+    });
+    $("#elem2").on("blur",function(){
+        console.log("blur");
+        $(this).css({"backgroundColor":"white"});
+    });
+    $(".line>textarea").on("keyup",(event)=>{console.log("keyup",event.originalEvent);});
+    $(".line>textarea").on("keydown",(event)=>{console.log("keydown",event.originalEvent);});
+    $(".line>textarea").on("keypress",(event)=>{console.log("keypress",event.originalEvent);});
 });
