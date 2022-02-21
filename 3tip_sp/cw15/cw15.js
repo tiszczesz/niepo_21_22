57b2128a7be9803b3e6e701512f497f2ad7d293b
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-02-21 12:51:55 +0100

    ee

--- 3tip_sp/cw15/cw15.js
+++ 3tip_sp/cw15/cw15.js
@@ -5,7 +5,10 @@ document.querySelector('#area').onfocus = function () {
 document.querySelector('#area').onblur = function () {
   console.log('blur');
   this.style.backgroundColor = 'white';
-  this.value = this.value.toUpperCase();
+  //this.value = this.value.toUpperCase();
+  document.querySelector(".result").innerHTML =
+       `<p>Ilość znaków: ${this.value.length}</p>`
+       +`<p>Ilość liter: ${CountLetter(this.value)}</p>`;
 };
 document.querySelector('#area').onkeyup = function (event) {
   console.log('keyup');
@@ -19,3 +22,19 @@ document.querySelector('#area').onkeypress = function (event) {
   console.log('keypress');
   console.log(event);
 };
+
+function isAlpha(char){
+  return typeof char ==='string' && char.length === 1
+        && ((char>='a' && char<='z') || (char>='A' && char<='Z'));
+}
+function isAlpha2(char){ //sprawdza polskie litery
+  return /^[a-ząęśćżźółń]$/i.test(char);
+}
+function CountLetter(text){
+  let count = 0;
+  for(let i=0;i<text.length;i++){
+    if(isAlpha2(text[i])) count++;
+  }
+  return count;
+}
+
