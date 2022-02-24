204b5fac80a6f5cd880af1e5f1936a30171c409a
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-02-24 14:37:47 +0100

    dd

--- uu/python/p1/p1.py
+++ uu/python/p1/p1.py
@@ -1,5 +1,7 @@
 from distutils.log import error
+import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 import sqlite3
 
 with sqlite3.connect("details.db") as db:
@@ -21,10 +23,22 @@ def add_new_user():
         cursor.execute("INSERT INTO users(username,password) VALUES(?,?)",(newUsername,newPassword))
         db.commit()
                        
-                       
+def select_user():
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    total = cursor.rowcount
+    for i in tv.get_children():
+       tv.delete(i)
+    window.update()
+    for i in rows:
+        tv.insert('','end',values=i)
+    
+                           
         
 window = Tk()
 window.geometry("450x280")
+window.state("zoomed")
+window.title = "Paraca z Sqlite"
 
 error = Message(text="",width=160)
 error.place(x=30, y=10)
@@ -41,4 +55,19 @@ password = Entry(text = "")
 password.place(x=150, y=80,width=200,height=25)
 button = Button(text="Dodaj",command=add_new_user)
 button.place(x=150, y=120,width=75,height=35)
+button = Button(text="Pokaż",command=select_user)
+button.place(x=150, y=220,width=75,height=35)
+
+frm = Frame(window)
+frm.pack(side=tk.RIGHT,padx=30,fill="both")
+
+tv = ttk.Treeview(frm,columns=(1,2,3),show="headings", height="20")
+vsb = ttk.Scrollbar(window,orient="vertical",command=tv.yview)
+vsb.pack(side ='right', fill ='both')
+tv.configure(yscrollcommand=vsb.set)
+tv.pack()
+tv.heading(1,text="lp")
+tv.heading(2,text="użytkownik")
+tv.heading(3, text="hasło")
+
 window.mainloop()
