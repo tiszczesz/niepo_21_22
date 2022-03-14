40f4d57bab9efcf95b2f25dfad0e1c07e994b2fe
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-03-14 13:16:53 +0100

    Update cw5.js

--- 3ti_gim_web/cwJS5/cw5.js
+++ 3ti_gim_web/cwJS5/cw5.js
@@ -7,4 +7,22 @@ $(function(){
         div.animate({'top':'+=50'},'slow');
         div.animate({'left':'+=700'},600);
     });
+    $("#start2").on('click',()=>{
+        const div = $('#second');
+        div.animate({'left':'+=400'},'slow');
+        div.animate({'top':'+=80'},2000);
+        div.animate({'left':'+=800'},800);
+    });
+    $('#start3').on('click',()=>{
+        $('#start1, #start2').click();
+    });
+    $('#stop').click(()=>{
+        const kulka = $('.kulka');
+        console.log(kulka);
+        kulka.clearQueue();
+        kulka.stop();
+    });
+    $('#reset').on('click',()=>{
+        $('#first, #second').css({left:'',top:''});
+    });
 });
