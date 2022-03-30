aa4595af2aec82458c01ec14bc02646a80339e70
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-03-30 16:01:47 +0200

    Update DerivedClassOne.h

--- 3tiGim_obiek/VirtualFun/VirtualFun/DerivedClassOne.h
+++ 3tiGim_obiek/VirtualFun/VirtualFun/DerivedClassOne.h
@@ -2,8 +2,13 @@
 #include "BaseClass.h"
 class DerivedClassOne :    public BaseClass
 {
+private:
+	bool isOK = false;
 public:
 	DerivedClassOne() {  }
+	DerivedClassOne(int fromBase,bool isOk):BaseClass(fromBase) {
+		this->isOK = isOk;
+	}
 	void ToOverride() {
 		std::cout << "Nadpisana w klasie DerivedClassOne.." << std::endl;
 	}
