0dccd3915e7c9620ad39f8a59a0a33004380efa8
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-04-12 15:34:50 +0200

    Update cw19.js

--- 3tip_sp/cw19/cw19.js
+++ 3tip_sp/cw19/cw19.js
@@ -1,24 +1,40 @@
-let date = new Date('2022-04-17');
+let date = new Date();
 function ShowTimer(elem) {
   let time = new Date();
   elem.innerHTML = time.toLocaleTimeString();
 }
-const elem = document.querySelector(".timer");
+const elem = document.querySelector('.timer');
 ShowTimer(elem);
 //setInterval(function(){ShowTimer(elem);}, 1000);
 setInterval(() => ShowTimer(elem), 1000);
 const months = [
-  "Styczeń",
-  "Luty",
-  "Marzec",
-  "Kwiecień",
-  "Maj",
-  "Czerwiec",  "Lipiec",  "Sierpień",  "Wrzesień",  "Październik",  "Listopad",  "Grudzień",
+  'Styczeń',
+  'Luty',
+  'Marzec',
+  'Kwiecień',
+  'Maj',
+  'Czerwiec',
+  'Lipiec',
+  'Sierpień',
+  'Wrzesień',
+  'Październik',
+  'Listopad',
+  'Grudzień',
 ];
 const days = [
-  "Niedziela",  "Poniedziałek",  "Wtorek",  "Środa",  "Czwartek",  "Piątek",  "Sobota",  
+  'Niedziela',
+  'Poniedziałek',
+  'Wtorek',
+  'Środa',
+  'Czwartek',
+  'Piątek',
+  'Sobota',
 ];
-document.querySelector(".year").innerHTML = date.getFullYear();
-document.querySelector(".month").innerHTML = months[date.getMonth()];
-document.querySelector(".date").innerHTML = date.getDate();
-document.querySelector(".day").innerHTML = days[date.getDay()];
+document.querySelector('.year').innerHTML = date.getFullYear();
+document.querySelector('.month').innerHTML = months[date.getMonth()];
+document.querySelector('.date').innerHTML = date.getDate();
+document.querySelector('.day').innerHTML = days[date.getDay()];
+if (date.getDay() == 0) document.querySelector('.day').style.color = 'red';
+else if (date.getDay() == 6)
+  document.querySelector('.day').style.color = 'green';
+else document.querySelector('.day').style.color = 'black';
