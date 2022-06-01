47ceebbc29eb2f2a9779798cb6a64f3b2ac917f3
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-06-01 11:37:10 +0200

    gg

---
+++ 3ti_gim_web/node_exercises/cw5/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './src/index.ts',
+    output: { filename: 'bundle.js' },
+    resolve: { extensions: ['.ts', '.js'] },
+    module: {
+      rules: [{ test: /\.ts/, use: 'ts-loader', exclude: /node_modules/ }],
+    },
+  };
